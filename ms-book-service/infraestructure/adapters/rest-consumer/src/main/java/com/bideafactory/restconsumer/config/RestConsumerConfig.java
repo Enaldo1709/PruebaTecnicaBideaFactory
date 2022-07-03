f2f7aded4b5e78614bc0aa072d5cad5b0ce77227
Enaldo Narvaez Yepes <enaldo_17@hotmail.com>
2022-07-03 01:09:08 -0500

    Added ssl public certificate

--- ms-book-service/infraestructure/adapters/rest-consumer/src/main/java/com/bideafactory/restconsumer/config/RestConsumerConfig.java
+++ ms-book-service/infraestructure/adapters/rest-consumer/src/main/java/com/bideafactory/restconsumer/config/RestConsumerConfig.java
@@ -3,6 +3,13 @@ package com.bideafactory.restconsumer.config;
 import static io.netty.channel.ChannelOption.CONNECT_TIMEOUT_MILLIS;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,15 +19,21 @@ import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.bideafactory.restconsumer.DiscountValidatorAdapter;
+
+import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.extern.slf4j.Slf4j;
 import reactor.netty.http.client.HttpClient;
-
+@Slf4j
 @Configuration
 public class RestConsumerConfig {
 
     @Value("${adapter.restconsumer.timeout}")
     private int timeout;
+    @Value("${adapter.restconsumer.ssl.keystore-pass}")
+    private String keyStorePass;
 
     @Bean
     WebClient getCient(){
@@ -32,6 +45,20 @@ public class RestConsumerConfig {
     }
     private ClientHttpConnector getClientHttpConnector() {
         return new ReactorClientHttpConnector(HttpClient.create()
+                .secure(context -> {
+                    try {
+                        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+                        trustStore.load(DiscountValidatorAdapter.class.getClassLoader()
+                        .getResourceAsStream("KeyStoreMockAPI"), keyStorePass.toCharArray());
+                        context.sslContext(
+                            SslContextBuilder.forClient()
+                                .trustManager((X509Certificate) trustStore.getCertificate("mockapi.io"))
+                                .build()
+                        );
+                    } catch (IOException | KeyStoreException | NoSuchAlgorithmException | CertificateException e) {
+                        log.error("Error while creating rest client: ", e);
+                    }
+                })
                 .compress(true)
                 .keepAlive(true)
                 .option(CONNECT_TIMEOUT_MILLIS, timeout)

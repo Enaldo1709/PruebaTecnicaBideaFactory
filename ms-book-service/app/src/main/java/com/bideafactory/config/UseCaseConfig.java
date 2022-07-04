56dce63bb7638336681f76f760d87bf1a59121be
Enaldo Narvaez Yepes <enaldo_17@hotmail.com>
2022-07-03 22:48:46 -0500

    Added use case bean creation

--- ms-book-service/app/src/main/java/com/bideafactory/config/UseCaseConfig.java
+++ ms-book-service/app/src/main/java/com/bideafactory/config/UseCaseConfig.java
@@ -1,15 +1,31 @@
 package com.bideafactory.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+
+import com.bideafactory.model.gateways.BookService;
+import com.bideafactory.model.gateways.BookValidator;
+import com.bideafactory.model.gateways.DiscountValidator;
+import com.bideafactory.usecase.ReservationsUseCase;
 
 @Configuration
-@ComponentScan(
-    basePackages = "com.bideafactory.usecase",
-    includeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.UseCase$"),
-    useDefaultFilters = false
-)
 public class UseCaseConfig {
-    
+    @Bean
+    ReservationsUseCase reservationsUseCase(
+            @Qualifier("bookEntityRespositoryAdapter") BookService repository,
+            BookValidator bookValidator, 
+            DiscountValidator discountValidator){
+        return new ReservationsUseCase(repository, bookValidator, discountValidator);
+    }
+
+    @Bean
+    DateFormat getDateFormat(){
+        return new SimpleDateFormat("yyyy-MM-dd");
+    }
+
 }
+
